4d86ec3b0c8b500b90cee54ea197aa8d10d15272
trzysiek <krzysztof.kiewicz@lizardfs.com>
2020-12-05 20:12:27 +0100

    Przetestowana wersja ze wszystkim poza ruchami damka i promocja pionow.
    * Poprawione wszystkie pomniejsze bugi.
    * Ladniejsze rysowanie planszy
    * Dodany test z pojedynczym, podwojnym biciem pionow. Dziala na nim.

--- kamila.java
+++ kamila.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Arrays; // TODO usunac
+
 public class kamila {
     long bPionki1;
     long bPionki2;
@@ -63,32 +66,34 @@ public class kamila {
 
     long ustawPozycje(long pionki, long przes, int x, int y) {
         // ustawia pozycje pionka z przesunieciem bitowym 'przes' na (x, y)                
-        long maskaPozycji = 0;
+        long maskaPozycji = 0L;
         for (int i = 0; i < 6; ++i)
-            maskaPozycji |= 1 << (przes + i);
+            maskaPozycji |= 1L << (przes + i);
         
         // najpierw zerujemy bity odpowiadające pozycji (x, y) pionka
         pionki &= ~maskaPozycji; 
         // potem ustawiamy odpowiednie wartości
-        pionki |= x << przes;
-        pionki |= y << (przes + 3);
+        pionki |= (long)x << przes;
+        pionki |= (long)y << (przes + 3);
         return pionki;
     }
 
     void zmienPozycjePiona(int x1, int y1, int x2, int y2) {
         // Zakładamy w tym miejscu, że ruch jest w 100% poprawny i możliwy do wykonania.
         // Sprawdzone wcześniej.
+        int a[] = {x1, y1, x2, y2};
+        System.out.println(Arrays.toString(a));
         for (int i = 0; i < ilePionkowNaLonga; ++i) {
             long przes = ileBitowNaPionka * i;
 
             if (czyPionekNaXY(bPionki1 >> przes, x1, y1))
-                bPionki1 = ustawPozycje(bPionki1, przes, x1, y1);
+                bPionki1 = ustawPozycje(bPionki1, przes, x2, y2);
             else if (czyPionekNaXY(bPionki2 >> przes, x1, y1))
-                bPionki2 = ustawPozycje(bPionki2, przes, x1, y1);
+                bPionki2 = ustawPozycje(bPionki2, przes, x2, y2);
             else if (czyPionekNaXY(czPionki1 >> przes, x1, y1))
-                czPionki1 = ustawPozycje(czPionki1, przes, x1, y1);
-            else if (czyPionekNaXY(czPionki1 >> przes, x1, y1))
-                czPionki1 = ustawPozycje(czPionki2, przes, x1, y1);
+                czPionki1 = ustawPozycje(czPionki1, przes, x2, y2);
+            else if (czyPionekNaXY(czPionki2 >> przes, x1, y1))
+                czPionki2 = ustawPozycje(czPionki2, przes, x2, y2);
         }
     }
 
@@ -96,7 +101,7 @@ public class kamila {
         // ustawiamy bit 'czyWGrze' na 0 -> pionek jest zbity.
         for (int i = 0; i < ilePionkowNaLonga; ++i) {
             long przes = ileBitowNaPionka * i;
-            long czyWGrzeBit = (1 << (przes + 8));
+            long czyWGrzeBit = (1L << (przes + 8));
 
             if (czyPionekNaXY(bPionki1 >> przes, x, y))
                 bPionki1 &= ~czyWGrzeBit;
@@ -104,8 +109,8 @@ public class kamila {
                 bPionki2 &= ~czyWGrzeBit;
             else if (czyPionekNaXY(czPionki1 >> przes, x, y))
                 czPionki1 &= ~czyWGrzeBit;
-            else if (czyPionekNaXY(czPionki1 >> przes, x, y))
-                czPionki1 &= ~czyWGrzeBit;
+            else if (czyPionekNaXY(czPionki2 >> przes, x, y))
+                czPionki2 &= ~czyWGrzeBit;
         } 
     }
 
@@ -167,7 +172,7 @@ public class kamila {
             return false;
 
         long bityPionek = pionekNaXY((x1 + x2) / 2, (y1 + y2) / 2);
-        return czyWGrze(bityPionek) || czyBialy(bityPionek) != turaBialego;
+        return czyWGrze(bityPionek) && czyBialy(bityPionek) != turaBialego;
     }
 
     boolean nieMaBiciaPionem(int x, int y, boolean turaBialego) {
@@ -177,16 +182,26 @@ public class kamila {
             && !mogeZbicPionem(x, y, x - 2, y - 2, turaBialego);
     }
 
+    int pom = 0;
+
     boolean biciePionem(int x1, int y1, int x2, int y2, boolean turaBialego) {
         // Zwraca true jeśli możliwa jest seria bić pionkiem z (x1, y1) prowadząca do
         // (x2, y2), przy aktualnej planszy (zmieniamy ją w trakcie kolejnych bić i
         // przywracamy do stanu początkowego, jeśli okazało się, że pewna sekwencja bić
         // to ślepy zaułek.
-        if (nieMaBiciaPionem(x1, y1, turaBialego) && x1 == x2 && y1 == y2)
+        pom++;
+        if (pom == 17)
+            return false;
+        int a[] = {x1, y1, x2, y2};
+        System.out.println(Arrays.toString(a));
+        if (nieMaBiciaPionem(x1, y1, turaBialego) && x1 == x2 && y1 == y2) {
             // wykonaliśmy całe poprawne bicie - koniec
+            System.out.println("KONIEC");
             return true;
+        }
 
         if (mogeZbicPionem(x1, y1, x1 + 2, y1 + 2, turaBialego)) {
+            System.out.println("TAK! W PRAWO W GÓRĘ");
             bijPionaIZaktualizujPlansze(x1, y1, x1 + 2, y1 + 2);
             if (biciePionem(x1 + 2, y1 + 2, x2, y2, turaBialego))
                 return true;
@@ -194,6 +209,7 @@ public class kamila {
         }
 
         if (mogeZbicPionem(x1, y1, x1 + 2, y1 - 2, turaBialego)) {
+            System.out.println("TAK! W PRAWO W DÓŁ");
             bijPionaIZaktualizujPlansze(x1, y1, x1 + 2, y1 - 2);
             if (biciePionem(x1 + 2, y1 - 2, x2, y2, turaBialego))
                 return true;
@@ -201,6 +217,7 @@ public class kamila {
         }
 
         if (mogeZbicPionem(x1, y1, x1 - 2, y1 + 2, turaBialego)) {
+            System.out.println("TAK! W LEWO W GÓRĘ");
             bijPionaIZaktualizujPlansze(x1, y1, x1 - 2, y1 + 2);
             if (biciePionem(x1 - 2, y1 + 2, x2, y2, turaBialego))
                 return true;
@@ -208,7 +225,9 @@ public class kamila {
         }
 
         if (mogeZbicPionem(x1, y1, x1 - 2, y1 - 2, turaBialego)) {
+            System.out.println("TAK! W LEWO W DÓŁ\nPlansza po biciu to:");
             bijPionaIZaktualizujPlansze(x1, y1, x1 - 2, y1 - 2);
+            rysujPlansze();
             if (biciePionem(x1 - 2, y1 - 2, x2, y2, turaBialego))
                 return true;
             cofnijAktualizacjePlanszyPoBiciuPionem(x1, y1, x1 - 2, y1 - 2, turaBialego);
@@ -245,15 +264,18 @@ public class kamila {
         int dy = y2 - y1;
         if (Math.abs(dx) == 1 && Math.abs(dy) == 1) {
             // zwykly ruch o 1 pole, bez bicia
-            boolean doGory = x2 > x1;
-            if (doGory != turaBialego)
+            boolean doDolu = y2 < y1;
+            if (doDolu != turaBialego)
                 // idziemy w zla strone
                 return false;
             if (poleZajete(x2, y2))
                 return false;
+            
+            // TODO jesli mamy bicie, to nie mozemy sie tak ruszyc!
+            
             zmienPozycjePiona(x1, y1, x2, y2);
-            if ((turaBialego && x2 == wlkPlanszy - 1) || (!turaBialego && x2 == 0))
-                promujPionaDoDamki(x2, y2);
+            //if ((turaBialego && x2 == wlkPlanszy - 1) || (!turaBialego && x2 == 0))
+            //    promujPionaDoDamki(x2, y2);
             return true;
         }
         else {
@@ -306,6 +328,11 @@ public class kamila {
             | (0422L << (przes * 3)) | (0424L << (przes * 4)) | (0426L << (przes * 5));
     }
 
+    boolean koniecGry() {
+        //TODO implement
+        return false;
+    }
+
     /******************************* RYSOWANIE ***************************************
      *********************************************************************************/
 
@@ -325,7 +352,12 @@ public class kamila {
     }
 
     void rysujPlansze() {
+        System.out.print(" ");
+        for (int i = 1; i <= wlkPlanszy; ++i)
+            System.out.print(i);
+        System.out.println();
         for (int y = wlkPlanszy - 1; y >= 0; --y) {
+            System.out.print(y + 1);
             for (int x = 0; x < wlkPlanszy; ++x) {
                 long pionek = pionekNaXY(x, y);
                 /*if (y == 0) {
@@ -337,13 +369,39 @@ public class kamila {
                 else
                     rysujPustePole(x, y);
             }
+            System.out.print(y + 1);
             System.out.println();
         }
+        System.out.print(" ");
+        for (int i = 1; i <= wlkPlanszy; ++i)
+            System.out.print(i);
+        System.out.println("\n-------------");
     }
 
     public static void main(String[] args) {
         kamila gra = new kamila();
         gra.ustawStartowePolozenie();
-        gra.rysujPlansze();
+        boolean turaBialego = true;
+        boolean czyRysowacPlansze = true;
+
+        Scanner scan = new Scanner(System.in);
+        while (!gra.koniecGry()) {
+            if (czyRysowacPlansze)
+                gra.rysujPlansze();
+            
+            int x1 = scan.nextInt();
+            int y1 = scan.nextInt();
+            int x2 = scan.nextInt();
+            int y2 = scan.nextInt();
+            if (!gra.ruch(x1 - 1, y1 - 1, x2 - 1, y2 - 1, turaBialego)) {
+                System.out.println("Bledny ruch!");
+                czyRysowacPlansze = false;
+            }
+            else {
+                turaBialego = !turaBialego;
+                czyRysowacPlansze = true;
+            }
+        }
+        scan.close();
     }
 }
